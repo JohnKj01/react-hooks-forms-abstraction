387cf29b901c719d79633cc4b25715e2d55bd8d1
John <johnkaranjairungu@gmail.com>
2023-04-22 08:09:39 +0300

    developed a submit event

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,45 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [formData, setFormData] = useState({
-    firstName: "John",
-    lastName: "Henry",
-    admin: false,
-  });
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([]);
 
-  function handleChange(event) {
-    const name = event.target.name;
-    let value = event.target.value;
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
 
-    // use `checked` property of checkboxes instead of `value`
-    if (event.target.type === "checkbox") {
-      value = event.target.checked;
-    }
-
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
   }
 
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="firstName"
-        onChange={handleChange}
-        value={formData.firstName}
-      />
-      <input
-        type="text"
-        name="lastName"
-        onChange={handleChange}
-        value={formData.lastName}
-      />
-      <input
-        type="checkbox"
-        name="admin"
-        onChange={handleChange}
-        checked={formData.admin}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
